f7e09e6255a7a09fcf31c1815e96845d49daf283
Sammy <Sammyhumes@gmail.com>
2017-03-10 12:34:27 -0800

    Added Comic Books List view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -59,7 +59,10 @@ namespace ComicBookGallery.Data
                 Favorite = false
             }
        };
-
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
